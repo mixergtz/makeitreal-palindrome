4874a824a5932c95a297bc43e697f4cd1cbd136d
Mixer Gutierrez <mixergutierrez@gmail.com>
2014-11-12 12:57:04 -0500

    Add some DRY

--- lib/palindrome.rb
+++ lib/palindrome.rb
@@ -1,7 +1,11 @@
-class Palindromes
+module Palindromes
 
   def self.is_palindrome?(string)
-    str_array = string.to_s.chars.to_a
+    string.is_palindrome?
+  end
+
+  def is_palindrome?
+    str_array = self.to_s.chars.to_a
     str_length = str_array.length
     i = 0
 
@@ -14,49 +18,14 @@ class Palindromes
         end
 
     end
-
   end
 
 end
 
 class String
-
-  def is_palindrome?
-    str_array = self.to_s.chars.to_a
-    str_length = str_array.length
-    i = 0
-
-    str_array.each do |char|
-    i += 1
-        if (char == str_array[str_length-i])
-            break true
-        else
-            break false
-        end
-
-    end
-
-  end
-
+    include Palindromes
 end
 
 class Integer
-
-  def is_palindrome?
-    str_array = self.to_s.chars.to_a
-    str_length = str_array.length
-    i = 0
-
-    str_array.each do |char|
-    i += 1
-        if (char == str_array[str_length-i])
-            break true
-        else
-            break false
-        end
-
-    end
-
-  end
-
+    include Palindromes
 end
